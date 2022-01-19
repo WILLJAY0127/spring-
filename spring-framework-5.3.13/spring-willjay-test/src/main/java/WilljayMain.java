0da0d0e7b245a2a27f1edafad6073102014a6642
WILLJAY <willjay0127@163.com>
2022-01-19 23:38:16 +0800

    添加xml方式的注入

--- spring-framework-5.3.13/spring-willjay-test/src/main/java/WilljayMain.java
+++ spring-framework-5.3.13/spring-willjay-test/src/main/java/WilljayMain.java
@@ -3,6 +3,7 @@ import beans.SysUser;
 import config.SysConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @Author lsc
@@ -12,7 +13,8 @@ public class WilljayMain {
 
 	public static void main(String[] args) {
 		// 获取容器
-		ApplicationContext ac =new AnnotationConfigApplicationContext(SysConfig.class);
+//		ApplicationContext ac =new AnnotationConfigApplicationContext(SysConfig.class);
+		ApplicationContext ac = new ClassPathXmlApplicationContext("BeanConfig.xml");
 		// 获取 bean
 		SysUser user = (SysUser) ac.getBean("sysUser");
 		System.out.println("111");
