d0435f2102320f7894f99418e87effa597d75242
aweijian_v <aweijian_v@didiglobal.com>
2022-01-20 12:36:49 +0800

    xml启动方式测试完成

--- spring-framework-5.3.13/spring-willjay-test/src/main/java/beans/SysUser.java
+++ spring-framework-5.3.13/spring-willjay-test/src/main/java/beans/SysUser.java
@@ -14,4 +14,28 @@ public class SysUser {
 		this.password = password;
 	}
 	// 省略set/get  toString
+
+	@Override
+	public String toString() {
+		return "SysUser{" +
+				"username='" + username + '\'' +
+				", password='" + password + '\'' +
+				'}';
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
